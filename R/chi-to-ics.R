08595ba44fda9db2654b636e035c44a8e3e3a269
Chat Wacharamanotham <chat@ifi.uzh.ch>
2021-05-08 17:32:37 +0200

    extract function to process CHI schedule export

--- R/chi-to-ics.R
+++ R/chi-to-ics.R
@@ -47,6 +47,7 @@ df_sessions <-
   left_join(df_timeslots, by = c("time_slot_id" = "id")) %>% 
   unnest(content_ids, keep_empty = TRUE) %>% 
   select(-type, -time_slot_id)
+tidy_schedule <- function(path_schedule_csv, tz_target) {
   
 
 df_contents <-
@@ -60,36 +61,37 @@ df_contents <-
 #===============================================================================
 # process the exporteds schedule
 
-df_chi_export <- 
-  read_csv(path_schedule_csv, col_types = "cccc___c") %>% 
-  rename_with(to_snake_case) %>% 
-  filter(!is.na(start_date)) %>%    # remove the "reading list" bookmarks
-  mutate(across(ends_with("_date"), ~ymd_hm(.x, tz = tz_source))) 
-
-# 1. match with the whole CHI content by title
-df_by_content <-
-  df_chi_export %>% 
-  filter(!is.na(content_title)) %>%
-  left_join(df_contents, by = "content_title", suffix = c("", ".y"))
-
-# 2. match with the whole CHI content by session name (when title doesn't exist)
-df_by_session <-
-  df_chi_export %>% 
-  filter(is.na(content_title)) %>% 
-  left_join(df_sessions, by = "session_name", suffix = c("", ".y")) %>% 
-  select(-content_ids)
-
-# combine 1. and 2. and filter duplicates due to session-repeats (specific to CHI 2021)
-df_schedule_src <- 
-  bind_rows(df_by_content, df_by_session) %>% 
-  filter(abs(start_date - start_date.y) < 360)  %>% # remove the entries that has the start time differs more than 360 seconds
-  select(-ends_with(".y"))
+  df_chi_export <- 
+    read_csv(path_schedule_csv, col_types = "cccc___c") %>% 
+    rename_with(to_snake_case) %>% 
+    filter(!is.na(start_date)) %>%    # remove the "reading list" bookmarks
+    mutate(across(ends_with("_date"), ~ymd_hm(.x, tz = tz_source))) 
   
-# convert to the target timezone
-df_target_tz <-
-  df_schedule_src %>% 
-  mutate(across(ends_with("_date"), ~with_tz(.x, tz_target), .names = "{.col}_target"))
+  # 1. match with the whole CHI content by title
+  df_by_content <-
+    df_chi_export %>% 
+    filter(!is.na(content_title)) %>%
+    left_join(df_contents, by = "content_title", suffix = c("", ".y"))
   
+  # 2. match with the whole CHI content by session name (when title doesn't exist)
+  df_by_session <-
+    df_chi_export %>% 
+    filter(is.na(content_title)) %>% 
+    left_join(df_sessions, by = "session_name", suffix = c("", ".y")) %>% 
+    select(-content_ids)
+  
+  # combine 1. and 2. and filter duplicates due to session-repeats (specific to CHI 2021)
+  df_schedule_src <- 
+    bind_rows(df_by_content, df_by_session) %>% 
+    filter(abs(start_date - start_date.y) < 360)  %>% # remove the entries that has the start time differs more than 360 seconds
+    select(-ends_with(".y"))
+    
+  # convert to the target timezone
+  df_target_tz <-
+    df_schedule_src %>% 
+    mutate(across(ends_with("_date"), ~with_tz(.x, tz_target), .names = "{.col}_target"))
+
+}
 
 write_ics <- function(chi_df, path_output) {
   df_ics <- 
@@ -119,8 +121,8 @@ write_ics <- function(chi_df, path_output) {
     write_file(path_output)
 }
   
-  
-  
+df_target_tz <- tidy_schedule(path_schedule_csv, tz_target)
+
 df_target_tz %>%   
   write_ics("output/all_schedule.ics")
 
